2fec86e597ef6180baf7e95b7f388dfdafa84fe6
Daniel Diaz <alu0100882186@ull.edu.es>
2017-11-09 09:02:35 +0000

    Pruebas realizadas y fallo

--- lib/alimento/alimento_class.rb
+++ lib/alimento/alimento_class.rb
@@ -1,26 +1,53 @@
 class Alimentos
+    
     attr_reader :nombre, :proteinas, :glucidos, :grasas
+    
     def initialize(nombre, proteinas, glucidos, grasas)
+        
         @nombre = nombre
         @glucidos = glucidos
         @proteinas = proteinas
         @grasas = grasas
+        
     end
     
     def valor_energ
+        
         aux_proteinas = @proteinas
         aux_glucidos = @glucidos
         aux_grasas = @grasas 
         v_energetico = 4* aux_glucidos + 4*aux_proteinas + 9*aux_grasas
         v_energetico
+        
     end
     
     def to_s
+        
         out = "\t\t\tProteinas\tLipidos\t\tGrasas\n"
         out << "#{@nombre}\t\t"
         out << "#{@proteinas}\t\t"
         out << "#{@glucidos}\t\t"
         out << "#{@grasas}"
         out
+        
+    end
+end
+
+class GrupoAlimento < Alimentos
+   
+   attr_reader :grupo
+   
+    def initialize(alimento, grupo)
+    
+        super(alimento.nombre, alimento.proteinas, alimento.glucidos, alimento.grasas)
+        @grupo = grupo
+    
+    end
+    
+    def to_s
+       
+       out = "#{@grupo}\n" + super.to_s
+       out
+    
     end
 end
--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -4,9 +4,28 @@ require 'lib/alimento/alimento_class'
 
 
 
-describe Alimentos do
+describe GrupoAlimento do
     before :each do
         @huevo_frito = Alimentos.new("Huevo frito", 14.1 , 0.0, 19.5)
+        @lechevaca = Alimentos.new("Leche vaca", 3.3, 4.8, 3.2)
+        @yogurt = Alimentos.new("Yogurt", 3.8, 4.9, 3.8)
+        @cerdo = Alimentos.new("Cerdo", 21.5, 0.0, 6.3)
+        @ternera = Alimentos.new("Ternera", 21.1, 0.0, 3.1)
+        @pollo = Alimentos.new("Pollo", 20.6, 0.0, 5.6)
+        @bacalao = Alimentos.new("Bacalao", 17.7, 0.0, 0.4)
+        @atun = Alimentos.new("Atun", 21.5, 0.0, 15.5)
+        @salmon = Alimentos.new("Salmon", 19.9, 0.0, 13.6)
+        @aceiteoliva = Alimentos.new("Aceite de oliva", 0.0, 0.2, 99.6)
+        @chocolate = Alimentos.new("Chocolate", 5.3, 47.0, 30.0)
+        @azucar = Alimentos.new("Azucar", 0.0, 99.8, 0.0)
+        @arroz = Alimentos.new("Arroz", 6.8, 77.7, 0.6)
+        @lentejas = Alimentos.new("Lentejas", 23.5, 52.0, 1.4)
+        @papas = Alimentos.new("Papas", 2.0, 15.4, 0.1)
+        @tomate = Alimentos.new("Tomate", 1.0, 3.5, 0.2)
+        @cebolla = Alimentos.new("Cebolla", 1.3, 5.8, 0.3)
+        @manzana = Alimentos.new("Manzana", 0.3, 12.4, 0.4)
+        @platanos = Alimentos.new("Platanos", 1.2, 21.4, 0.2)
+        @grupo = GrupoAlimento.new(@lechevaca,"Lacteos")
     end
     
     describe "Almacenamiento correcto de los alimentos" do
